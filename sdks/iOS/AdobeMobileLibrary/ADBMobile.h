995a4704b7cd69add3b59ae24656721847054ef8
jgeng <jgeng@adobe.com>
2015-05-06 10:47:23 -0600

    iOS 4.5.2 release

--- sdks/iOS/AdobeMobileLibrary/ADBMobile.h
+++ sdks/iOS/AdobeMobileLibrary/ADBMobile.h
@@ -4,7 +4,7 @@
 //
 //  Copyright 1996-2015. Adobe, Inc. All Rights Reserved
 //
-//  SDK Version: 4.5.1
+//  SDK Version: 4.5.2
 
 #import <Foundation/Foundation.h>
 @class CLLocation, CLBeacon, ADBTargetLocationRequest, ADBMediaSettings, ADBMediaState;
