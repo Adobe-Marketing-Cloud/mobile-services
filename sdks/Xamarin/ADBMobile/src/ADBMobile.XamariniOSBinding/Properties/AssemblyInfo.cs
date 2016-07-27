01049a5bf3d26e6c38cf59d4b72693d2d7ff36f6
Alexandre Zollinger Chohfi <azchohfi@hotmail.com>
2016-07-27 16:56:34 -0300

    Updated to 4.11.0.

--- sdks/Xamarin/ADBMobile/src/ADBMobile.XamariniOSBinding/Properties/AssemblyInfo.cs
+++ sdks/Xamarin/ADBMobile/src/ADBMobile.XamariniOSBinding/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
 using Foundation;
 
 // This attribute allows you to mark your assemblies as “safe to link”.
@@ -25,7 +23,7 @@ using Foundation;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("4.4.0")]
+[assembly: AssemblyVersion ("4.11.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
