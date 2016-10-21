3b792fa229e243896d3574b902e9a75f938ee3b9
AdobeMobileTesting <Adobe-Mobile-Testing@adobe.com>
2016-10-20 18:50:27 -0600

    realease iOS version 4.13.1

--- sdks/iOS/AdobeMobileLibrary/ADBMobile.h
+++ sdks/iOS/AdobeMobileLibrary/ADBMobile.h
@@ -4,7 +4,7 @@
 //
 //  Copyright 1996-2016. Adobe, Inc. All Rights Reserved
 //
-//  SDK Version: 4.13.0
+//  SDK Version: 4.13.1
 
 #import <Foundation/Foundation.h>
 @class CLLocation, CLBeacon, TVApplicationController, ADBTargetLocationRequest, ADBMediaSettings, ADBMediaState;
