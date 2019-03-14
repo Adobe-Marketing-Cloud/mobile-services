1b4ce77bc7ca100ff585d83d111032b42e4dcac5
Emilia Dobrin <dobrin@adobe.com>
2019-03-14 09:59:58 -0700

    Android sdk v4.17.3 and iOS sdk v4.18.1 release

--- sdks/iOS/AdobeMobileLibrary/ADBMobile.h
+++ sdks/iOS/AdobeMobileLibrary/ADBMobile.h
@@ -4,7 +4,7 @@
 //
 //  Copyright 1996-2018. Adobe, Inc. All Rights Reserved
 //
-//  SDK Version: 4.18.0
+//  SDK Version: 4.18.1
 
 #import <Foundation/Foundation.h>
 @class CLLocation, CLBeacon, TVApplicationController,
