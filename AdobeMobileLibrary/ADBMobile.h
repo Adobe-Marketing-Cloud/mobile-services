d118786b9548d04fe5df2420b722eed8533b61cf
jgeng <jgeng@adobe.com>
2019-09-20 15:10:29 -0600

    Release 4.18.8

--- AdobeMobileLibrary/ADBMobile.h
+++ AdobeMobileLibrary/ADBMobile.h
@@ -4,7 +4,7 @@
 //
 //  Copyright 1996-2019. Adobe, Inc. All Rights Reserved
 //
-//  SDK Version: 4.18.7
+//  SDK Version: 4.18.8
 
 #import <Foundation/Foundation.h>
 @class CLLocation, CLBeacon, TVApplicationController,
